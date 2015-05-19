c7d67bcde1f0ff4faaf82e7ce573f3fe50051185
Halodew <stephenduehlmeier@gmail.com>
2015-05-19 09:52:51 -0600

    5/19/15 9:51am
    added target

--- FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure.cpp
+++ FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure.cpp
@@ -13,9 +13,7 @@ ifstream gamesaveIN ("C:/Users/duehlste001/Documents/Visual Studio 2013/Projects
 ofstream gamesaveOUT; //("C:/Users/duehlste001/Documents/Visual Studio 2013/Projects/FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure/GameSave.txt");
 
 
-
-
-void Attack()
+int Attack(int target)
 {
 	Player Player1;
 	int sword = 15;
@@ -29,75 +27,131 @@ void Attack()
 	if (Player1.WeaponSelect == "bow" || Player1.WeaponSelect == "Bow")
 	{
 		bow = Player1.BowWeaponUse(bow, Player1.WeaponSelect);
+		target = target - 10;
 	}
 	else if (Player1.WeaponSelect == "sword" || Player1.WeaponSelect == "Sword")
 	{
 		sword = Player1.SwordWeaponUse(sword, Player1.WeaponSelect);
+		target = target -15;
 	}
-	cout << bow << endl;
-	cout << sword << endl;
+	cout << "bow uses left: " << bow << endl;
+	cout << "sword uses left: " << sword << endl;
 
 	cout << endl;
+
+	return target;
 }
 
-void gamesave(int health)
+void Battle(int target)
+{
+	
+	cout << "your walking down a dark path when a bear attackes you" << endl;
+	while (target > 0)
+	{
+		cout << "bears health: " << target << endl;
+		cout << endl;
+		target = Attack(target);
+	}
+	if (target <= 0)
+	{
+		cout << "you have killed the bear." << endl;
+	}
+}
+
+void MainGame(int Health, int target)
+{
+	cout << endl;
+	cout << endl;
+
+	cout << "Current Health " << Health << endl;
+	if (Health == 0)
+	{
+		cout << "Game Over" << endl;
+		cout << "You have died" << endl;
+	}
+	else if (Health > 0)
+	{
+		Battle(target);
+	}
+
+}
+
+void SaveGame(int health, int target)
 {
 	gamesaveOUT.open("C:/Users/duehlste001/Documents/GitHub/Jatc-FinalProject/FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure/GameSave.txt");
-	gamesaveOUT << "current health: " << health;
-	cout << health << endl;
+	gamesaveOUT << "current_health: " << health << endl;
+	cout << "Game Saved" << endl;
+
+	gamesaveOUT << "targets_current_health: " << target << endl;
+
 	gamesaveOUT.close();
 }
 
 int LoadGame()
 {
-	int health = 100;
-	string health1;
-	int health2;
+	
+	string HealthText;
+	int Health;
+
 	gamesaveIN.open("C:/Users/duehlste001/Documents/GitHub/Jatc-FinalProject/FinalProject_TextBasedAdventure/FinalProject_TextBasedAdventure/GameSave.txt");
 	if(gamesaveIN.is_open() == true)
 	{
 		cout << "game loaded" << endl;
-		cout << health << endl;
-		gamesaveIN >> health1;
-		cout << health1 << " ";
+	
+		gamesaveIN >> HealthText;
+		cout << HealthText << " ";
 
-		gamesaveIN >> health2;
-		cout << health2 << endl;
+		gamesaveIN >> Health;
+		cout << Health << endl;
+
+		gamesaveIN.close();
 	}
 	else
 	{
 		cout << "Unable to load game save" << endl;
 	}
-	return health;
+	return Health;
 }
 
-void MainMenu()
+int MainMenu(int target)
 {
 	int LoadOption;
+	int Health;
 	cout << "	Welcome to the Main Menu	" << endl;
 	cout << endl;
+	
 	cout << "1: Load Game" << endl;
 	cout << "2: New Game" << endl;
+	cout << endl;
+
+	cout << "Enter: ";
 	cin >> LoadOption;
+	cout << endl;
 
 	if (LoadOption == 1)
 	{
-		LoadGame();
+		Health = LoadGame();
 	}
 	else if (LoadOption == 2)
 	{
-		cout << "test2" << endl;
+		Health = 100;
+		target = 150;
+		SaveGame(Health, target);
 	}
-
+	return Health;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int health = 10;
+	int health;
+	int target;
 
-	MainMenu();
+	health = MainMenu(target);
+	
+	MainGame(health,target);
+	
 
-	//gamesave(health);
+	//SaveGame(health);
 	return 0;
 }
 
